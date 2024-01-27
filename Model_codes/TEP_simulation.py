82443587121fc422b362066c0e7ee73daab7198b
Kerem Ziya Akdemir <56842236+keremakdemir@users.noreply.github.com>
2024-01-27 13:05:13 -0500

    Initial model upload

---
+++ Model_codes/TEP_simulation.py
@@ -0,0 +1,314 @@
+import os
+import pandas as pd
+import numpy as np
+import pyomo.environ as pyo
+
+#Reading all necessary datasets
+Generators_df = pd.read_csv("Inputs/data_genparams.csv", header=0)
+Fuel_Prices_df = pd.read_csv("Inputs/Fuel_prices.csv", header=0)
+Gen_Node_Matrix_df = pd.read_csv("Inputs/gen_mat.csv", header=0)
+Hydro_max_df = pd.read_csv("Inputs/Hydro_max.csv", header=0)
+Hydro_min_df = pd.read_csv("Inputs/Hydro_min.csv", header=0)
+Mustrun_df = pd.read_csv("Inputs/must_run.csv", header=0)
+Demand_df = pd.read_csv("Inputs/nodal_load.csv", header=0)
+Solar_df = pd.read_csv("Inputs/nodal_solar.csv", header=0)
+Wind_df = pd.read_csv("Inputs/nodal_wind.csv", header=0)
+Offshore_Wind_df = pd.read_csv("Inputs/nodal_offshorewind.csv", header=0)
+Line_Params_df = pd.read_csv("Inputs/line_params.csv", header=0)
+Line_Node_Matrix_df = pd.read_csv("Inputs/line_to_bus.csv", header=0)
+Line_Node_Matrix_df = Line_Node_Matrix_df.iloc[:,1:]
+
+#Defining renewable and thermal generators
+Renewable_Generators = Generators_df.loc[Generators_df['typ'].isin(['solar', 'wind','offshorewind','hydro'])].copy()
+Thermal_Generators = Generators_df.loc[~Generators_df['typ'].isin(['solar', 'wind','offshorewind','hydro'])].copy()
+
+#Reformatting generation-node matrix to exclude renewable generators
+Gen_Node_Matrix_df = Gen_Node_Matrix_df.loc[Gen_Node_Matrix_df['name'].isin(Thermal_Generators['name'])]
+Gen_Node_Matrix_df = Gen_Node_Matrix_df.iloc[:,1:]
+
+#Defining the number of days in every month
+Days_in_months = np.array([31,28,31,30,31,30,31,31,30,31,30,31])
+#Defining the number of hours in every month
+Hours_in_months = Days_in_months*24
+
+#Designating number of generators, nodes, lines and demand periods (months of the year = 12)
+Num_Thermal_Gens = Thermal_Generators.shape[0]
+Num_Nodes = Demand_df.shape[1]
+Num_Lines = Line_Params_df.shape[0]
+Num_Periods = len(Hours_in_months)
+
+#Determining indices
+Thermal_Gens = np.array([*range(0,Num_Thermal_Gens)]) 
+Nodes = np.array([*range(0,Num_Nodes)])
+Lines = np.array([*range(0,Num_Lines)])
+Periods = np.array([*range(0,Num_Periods)])
+
+#Saving important information as arrays for easy/fast referencing afterwards
+Thermal_Gen_Names = Thermal_Generators["name"].values
+Thermal_Gen_Types = Thermal_Generators["typ"].values
+Thermal_Gen_Node = Thermal_Generators["node"].values
+Thermal_Gen_MaxCap = Thermal_Generators["maxcap"].values
+Thermal_Gen_HeatRate = Thermal_Generators["heat_rate"].values
+Thermal_Gen_VarOM = Thermal_Generators["var_om"].values
+
+Thermal_Fuel_Prices = Fuel_Prices_df.values
+Gen_Node_Matrix = Gen_Node_Matrix_df.values
+
+Node_Names = Demand_df.columns.values
+Hydro_Max = Hydro_max_df.values
+Hydro_Min = Hydro_min_df.values
+Mustrun = Mustrun_df.values
+Demand = Demand_df.values
+Solar = Solar_df.values
+Wind = Wind_df.values
+Offshore_Wind = Offshore_Wind_df.values
+Line_Node_Matrix = Line_Node_Matrix_df.values
+
+Line_Names = Line_Params_df["line"].values
+Line_Reactances = Line_Params_df["reactance"].values
+Line_Initial_Limits = Line_Params_df["limit"].values
+Line_Types = Line_Params_df["transmission_type"].values
+Line_Lengths = Line_Params_df["length_mile"].values
+Line_Costs = Line_Params_df["inv_cost_$_per_MWmile"].values
+
+############################ TRANSMISSION EXPANSION MODEL ############################
+
+solver = "gurobi"
+
+#Defining solver
+opt = pyo.SolverFactory(solver)
+
+#Creating empty arrays to store model results
+ThermalGeneration_Results = np.zeros((Num_Periods, Num_Thermal_Gens))
+SolarGeneration_Results = np.zeros((Num_Periods, Num_Nodes))
+WindGeneration_Results = np.zeros((Num_Periods, Num_Nodes))
+OffWindGeneration_Results = np.zeros((Num_Periods, Num_Nodes))
+HydroGeneration_Results = np.zeros((Num_Periods, Num_Nodes))
+PowerFlow_Results = np.zeros((Num_Periods, Num_Lines))
+UnservedEnergy_Results = np.zeros((Num_Periods, Num_Nodes))
+VoltageAngle_Results = np.zeros((Num_Periods, Num_Nodes))
+LineLimit_Results = np.zeros(Num_Lines)
+
+#Initializing the model as concrete model
+m=pyo.ConcreteModel()
+
+#Telling model that we will need duals to check shadow prices for relevant constraints
+m.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
+
+#Defining sets
+m.G=pyo.Set(initialize=Thermal_Gens) #All thermal generators
+m.N=pyo.Set(initialize=Nodes) #All nodes
+m.L=pyo.Set(initialize=Lines) #All transmission lines
+m.T=pyo.Set(initialize=Periods) #All demand periods
+
+#Defining decision variables
+m.ThermalGen = pyo.Var(m.G, m.T, within=pyo.NonNegativeReals, initialize=0) #Generation from thermal generators in MWh
+m.SolarGen = pyo.Var(m.N, m.T, within=pyo.NonNegativeReals, initialize=0) #Generation from solar generators in MWh
+m.WindGen = pyo.Var(m.N, m.T, within=pyo.NonNegativeReals, initialize=0) #Generation from wind generators in MWh
+m.OffWindGen = pyo.Var(m.N, m.T, within=pyo.NonNegativeReals, initialize=0) #Generation from offshore wind generators in MWh
+m.HydroGen = pyo.Var(m.N, m.T, within=pyo.NonNegativeReals, initialize=0) #Generation from hydro generators in MWh
+
+m.ActualFlow = pyo.Var(m.L, m.T, within=pyo.Reals, initialize=0) #Actual power flow on transmission lines in MWh
+m.DummyFlow = pyo.Var(m.L, m.T, within=pyo.Reals, initialize=0) #Absolute value of power flow on transmission lines in MWh
+m.NewLineCap = pyo.Var(m.L, within=pyo.PositiveReals) #New capacity of each transmission line in MW
+
+m.LossOfLoad = pyo.Var(m.N, m.T, within=pyo.NonNegativeReals, initialize=0) #Unserved energy in MWh
+m.VoltAngle = pyo.Var(m.N, m.T, within=pyo.Reals, bounds=(-180, 180)) #Voltage angle in degrees
+
+#Defining objective function
+def TEPCost(m):
+
+    #Generation cost from dispatchable thermal generators
+    Gen_cost = sum(m.ThermalGen[g,t]*((Thermal_Gen_HeatRate[g]*Thermal_Fuel_Prices[t,g])+Thermal_Gen_VarOM[g])*Hours_in_months[t] for g in m.G for t in m.T)
+    #Loss of load (i.e., unserved energy cost)
+    LOL_cost = sum(m.LossOfLoad[n,t]*2000*Hours_in_months[t] for n in m.N for t in m.T)
+    #Generation cost from solar generators
+    Solar_cost = sum(m.SolarGen[n,t]*0.01*Hours_in_months[t] for n in m.N for t in m.T)
+    #Generation cost from wind generators
+    Wind_cost = sum(m.WindGen[n,t]*0.01*Hours_in_months[t] for n in m.N for t in m.T)
+    #Generation cost from offshore wind generators
+    OffWind_cost = sum(m.OffWindGen[n,t]*0.01*Hours_in_months[t] for n in m.N for t in m.T)
+    #Generation cost from hydro generators
+    Hydro_cost = sum(m.HydroGen[n,t]*0.01*Hours_in_months[t] for n in m.N for t in m.T)
+    #Power flow cost on transmission lines
+    Power_flow_cost = sum(m.DummyFlow[l,t]*0.01*Hours_in_months[t] for l in m.L for t in m.T)
+    #Investment cost for transmission capacity additions
+    Tr_investment_cost = sum((m.NewLineCap[l]-Line_Initial_Limits[l])*Line_Lengths[l]*Line_Costs[l] for l in m.L)
+
+    return Gen_cost + LOL_cost + Solar_cost + Wind_cost + OffWind_cost + Hydro_cost + Power_flow_cost + Tr_investment_cost
+    
+m.ObjectiveFunc=pyo.Objective(rule=TEPCost, sense=pyo.minimize)
+
+#Defining constraints
+
+#Maximum capacity constraint for thermal generators
+def MaxC(m,g,t):
+    return m.ThermalGen[g,t] <= Thermal_Gen_MaxCap[g]
+m.ThermalMaxCap_Cons= pyo.Constraint(m.G, m.T, rule=MaxC)
+
+#Maximum capacity constraint for solar generators
+def SolarMax(m,n,t):
+    return m.SolarGen[n,t] <= Solar[t,n]
+m.SolarMaxCap_Cons= pyo.Constraint(m.N, m.T, rule=SolarMax)
+
+#Maximum capacity constraint for wind generators
+def WindMax(m,n,t):
+    return m.WindGen[n,t] <= Wind[t,n]
+m.WindMaxCap_Cons= pyo.Constraint(m.N, m.T, rule=WindMax)
+
+#Maximum capacity constraint for offshore wind generators
+def OffWindMax(m,n,t):
+    return m.OffWindGen[n,t] <= Offshore_Wind[t,n]
+m.OffshoreWindMaxCap_Cons= pyo.Constraint(m.N, m.T, rule=OffWindMax)
+
+#Maximum capacity constraint for hydro generators
+def HydroMax(m,n,t):
+    return m.HydroGen[n,t] <= Hydro_Max[t,n]
+m.HydroMaxCap_Cons= pyo.Constraint(m.N, m.T, rule=HydroMax)
+
+#Minimum capacity constraint for hydro generators
+def HydroMin(m,n,t):
+    return m.HydroGen[n,t] >= Hydro_Min[t,n]
+m.HydroMinCap_Cons= pyo.Constraint(m.N, m.T, rule=HydroMin)
+
+#Voltage angle constraint for reference node
+def ThetaRef(m,t):
+    return m.VoltAngle[0,t] == 0
+m.RefVoltAngle_Cons = pyo.Constraint(m.T, rule=ThetaRef)
+
+#Kirchhoff's Voltage Law constraint
+def KVL_Loop(m,l,t):
+    theta_diff = sum(m.VoltAngle[n,t]*Line_Node_Matrix[l,n] for n in m.N)
+    return  100*theta_diff == m.ActualFlow[l,t]*Line_Reactances[l]
+m.KVLAroundLoopConstraint = pyo.Constraint(m.L, m.T, rule=KVL_Loop)
+
+#Maximum transmission line flow constraint (positive side)
+def FlowUP(m,l,t):
+    return m.ActualFlow[l,t] <= m.NewLineCap[l]
+m.FlowUP_Cons = pyo.Constraint(m.L, m.T ,rule=FlowUP)
+
+#Maximum transmission line flow constraint (negative side)
+def FlowDOWN(m,l,t):
+    return -m.ActualFlow[l,t] <= m.NewLineCap[l]
+m.FlowDOWN_Cons = pyo.Constraint(m.L, m.T ,rule=FlowDOWN)
+
+#Minimum new line capacity constraint
+def NewLCapacity(m,l):
+    return Line_Initial_Limits[l] <= m.NewLineCap[l]
+m.NewLCapacity_Cons = pyo.Constraint(m.L, rule=NewLCapacity)
+
+#Constraint to find absolute value of flow (positive side)
+def DummyFlowUP(m,l,t):
+    return  m.DummyFlow[l,t] >= m.ActualFlow[l,t]
+m.DummyFlowUP_Cons = pyo.Constraint(m.L, m.T ,rule=DummyFlowUP)
+
+#Constraint to find absolute value of flow (negative side)
+def DummyFlowDOWN(m,l,t):
+    return  m.DummyFlow[l,t] >= -m.ActualFlow[l,t]
+m.DummyFlowDOWN_Cons = pyo.Constraint(m.L, m.T ,rule=DummyFlowDOWN)
+
+#Kirchhoff's Current Law (i.e., nodal power balance) constraint
+def KCL(m,n,t):
+    total_power_flow = sum(m.ActualFlow[l,t]*Line_Node_Matrix[l,n] for l in m.L)
+    total_thermal_gen = sum(m.ThermalGen[g,t]*Gen_Node_Matrix[g,n] for g in m.G) 
+    total_renewable_gen = m.SolarGen[n,t] + m.WindGen[n,t] + m.OffWindGen[n,t] + m.HydroGen[n,t]
+    mustrun_gen = Mustrun[t,n]
+    total_LOL = m.LossOfLoad[n,t]
+    return total_thermal_gen + total_renewable_gen + total_LOL + mustrun_gen - total_power_flow == Demand[t,n]
+m.KCL_Cons = pyo.Constraint(m.N, m.T, rule=KCL)
+
+#Calling the solver to solve the model
+TEP_results = opt.solve(m)
+
+#Checking the solver status and if solution is feasible or not
+if (TEP_results.solver.status == pyo.SolverStatus.ok) and (TEP_results.solver.termination_condition == pyo.TerminationCondition.optimal):
+    print('Success! Solution is feasible.') 
+elif (TEP_results.solver.termination_condition == pyo.TerminationCondition.infeasible):
+    print('Solution is INFEASIBLE!!!') 
+else:
+    print(f'Something else is not right, solver status is {TEP_results.solver.status}.')
+
+#Creating a folder to store the results
+os.makedirs('Outputs/TEP', exist_ok=True)
+
+#Saving and writing thermal generation results
+for g_n in Thermal_Gens:
+    for t_n in Periods:
+        ThermalGeneration_Results[t_n, g_n] = m.ThermalGen[g_n, t_n]()
+
+ThermalGeneration_Results = pd.DataFrame(ThermalGeneration_Results, columns=Thermal_Gen_Names)
+ThermalGeneration_Results.to_csv("Outputs/TEP/Thermal_Gen.csv", index=False)
+
+#Saving and writing solar generation results
+for n_n in Nodes:
+    for t_n in Periods:
+        SolarGeneration_Results[t_n, n_n] = m.SolarGen[n_n, t_n]()
+
+SolarGeneration_Results = pd.DataFrame(SolarGeneration_Results, columns=Node_Names)
+SolarGeneration_Results.to_csv("Outputs/TEP/Solar_Gen.csv", index=False)
+
+#Saving and writing wind generation results
+for n_n in Nodes:
+    for t_n in Periods:
+        WindGeneration_Results[t_n, n_n] = m.WindGen[n_n, t_n]()
+
+WindGeneration_Results = pd.DataFrame(WindGeneration_Results, columns=Node_Names)
+WindGeneration_Results.to_csv("Outputs/TEP/Wind_Gen.csv", index=False)
+
+#Saving and writing offshore wind generation results
+for n_n in Nodes:
+    for t_n in Periods:
+        OffWindGeneration_Results[t_n, n_n] = m.OffWindGen[n_n, t_n]()
+
+OffWindGeneration_Results = pd.DataFrame(OffWindGeneration_Results, columns=Node_Names)
+OffWindGeneration_Results.to_csv("Outputs/TEP/OffshoreWind_Gen.csv", index=False)
+
+#Saving and writing hydro generation results
+for n_n in Nodes:
+    for t_n in Periods:
+        HydroGeneration_Results[t_n, n_n] = m.HydroGen[n_n, t_n]()
+
+HydroGeneration_Results = pd.DataFrame(HydroGeneration_Results, columns=Node_Names)
+HydroGeneration_Results.to_csv("Outputs/TEP/Hydro_Gen.csv", index=False)
+
+#Saving and writing unserved energy results
+for n_n in Nodes:
+    for t_n in Periods:
+        UnservedEnergy_Results[t_n, n_n] = m.LossOfLoad[n_n, t_n]()
+
+UnservedEnergy_Results = pd.DataFrame(UnservedEnergy_Results, columns=Node_Names)
+UnservedEnergy_Results.to_csv("Outputs/TEP/Unserved_Energy.csv", index=False)
+
+#Saving and writing voltage angle results
+for n_n in Nodes:
+    for t_n in Periods:
+        VoltageAngle_Results[t_n, n_n] = m.VoltAngle[n_n, t_n]()
+
+VoltageAngle_Results = pd.DataFrame(VoltageAngle_Results, columns=Node_Names)
+VoltageAngle_Results.to_csv("Outputs/TEP/Voltage_Angle.csv", index=False)
+
+#Saving and writing power flow results
+for l_n in Lines:
+    for t_n in Periods:
+        PowerFlow_Results[t_n, l_n] = m.ActualFlow[l_n, t_n]()
+
+PowerFlow_Results = pd.DataFrame(PowerFlow_Results, columns=Line_Names)
+PowerFlow_Results.to_csv("Outputs/TEP/Power_Flow.csv", index=False)
+
+#Saving and writing new transmission line limit results
+for l_n in Lines:
+    LineLimit_Results[l_n] = m.NewLineCap[l_n]()
+
+LineLimit_Results = pd.DataFrame(LineLimit_Results, columns=["New_Capacity"])
+LineLimit_Results.insert(0, "Name", Line_Names)
+LineLimit_Results.insert(1, "Reactance", Line_Reactances)
+LineLimit_Results.insert(2, "Type", Line_Types)
+LineLimit_Results.insert(3, "Length", Line_Lengths)
+LineLimit_Results.insert(4, "Investment_Cost", Line_Costs)
+LineLimit_Results.insert(5, "Old_Capacity", Line_Initial_Limits)
+LineLimit_Results["Capacity_Addition"] = LineLimit_Results["New_Capacity"] - LineLimit_Results["Old_Capacity"]
+
+LineLimit_Results.to_csv("Outputs/TEP/New_Transmission_Data.csv", index=False)
+
+######################################################################################
+
---
+++ Model_setup.py
@@ -0,0 +1,297 @@
+import pandas as pd
+import numpy as np
+import os
+from shutil import copy
+from pathlib import Path
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+from geopy.distance import geodesic
+
+################## Defining model parameters ##################
+
+#GO parameters
+Nodes_no = 125 #Number of nodes in GO model
+Formulation = "simple" #Defining if GO uses LP or MILP
+Transmission_scaling = 500 #Transmission scaling factor of GO
+Hurdle_scaling = -100 #Hurdle rate scaling factor of GO
+
+#TEP parameters
+TEP_year = 2015 #Year to make transmission plans
+Climate_scenario = "rcp45cooler_ssp3" #TGW climate scenario 
+Interreginonal_TEP_penalty = 50 #Transmission investment cost penalty in % for the lines crossing transmission planning region boundaries
+Line_len_security_scalar = 30 #Line length adder in % for security purposes
+# Demand_security_scalar = 15 #Demand adder in % for security purposes
+
+###############################################################
+
+#Defining the number of days in every month
+Days_in_months = [31,28,31,30,31,30,31,31,30,31,30,31]
+#Defining the number of hours in every month
+Hours_in_months = [m*24 for m in Days_in_months]
+
+#Creating a simulation folder for TEP optimization
+TEP_case_name = f'{Nodes_no}_{Formulation}_{Transmission_scaling}_{Hurdle_scaling}_{Climate_scenario}_{TEP_year}_{Interreginonal_TEP_penalty}'
+path_1 = str(Path(f'{Path.cwd()}/Simulation_folders/TEP_{TEP_case_name}'))
+os.makedirs(path_1, exist_ok=True)
+
+path_2 = str(Path(f'{Path.cwd()}/Simulation_folders/TEP_{TEP_case_name}/Inputs'))
+os.makedirs(path_2, exist_ok=True)
+
+path_3 = str(Path(f'{Path.cwd()}/Simulation_folders/TEP_{TEP_case_name}/Outputs'))
+os.makedirs(path_3, exist_ok=True)
+
+#Reading and organizing generator data and copying to simulation folder
+GO_case_name = f'{Nodes_no}_{Formulation}_{Transmission_scaling}_{Hurdle_scaling}_{TEP_year}_{Climate_scenario}'
+
+gen_data = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/data_genparams.csv", header=0)
+gen_data_filt = gen_data.loc[:,["name","typ","node","maxcap","heat_rate","var_om"]].copy()
+gen_data_filt.to_csv(f"{path_2}/data_genparams.csv", index=False)
+
+#Copying generator/node matrix to simulation folder
+copy(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/gen_mat.csv", path_2)
+
+#Reading and calculating average fuel price and hydropower for each month and copying to simulation folder
+fuel_price = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/Fuel_prices.csv", header=0)
+fuel_price_monthly = pd.DataFrame(np.zeros((len(Days_in_months),fuel_price.shape[1])), columns=fuel_price.columns)
+
+hydro_min = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/Hydro_min.csv", header=0)
+hydro_min_monthly = pd.DataFrame(np.zeros((len(Days_in_months),hydro_min.shape[1])), columns=hydro_min.columns)
+
+hydro_max = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/Hydro_max.csv", header=0)
+hydro_max_monthly = pd.DataFrame(np.zeros((len(Days_in_months),hydro_max.shape[1])), columns=hydro_max.columns)
+
+day_counter = 0
+idx = 0
+for month_day in Days_in_months:
+
+    if idx == 0:
+        fuel_filt = fuel_price.loc[0:month_day-1,:].mean().values
+        fuel_price_monthly.loc[idx,:] = fuel_filt
+
+        hydro_min_filt = hydro_min.loc[0:month_day-1,:].mean().values
+        hydro_min_monthly.loc[idx,:] = hydro_min_filt
+
+        hydro_max_filt = hydro_max.loc[0:month_day-1,:].mean().values
+        hydro_max_monthly.loc[idx,:] = hydro_max_filt
+
+    else:
+
+        fuel_filt = fuel_price.loc[day_counter:day_counter+month_day-1,:].mean().values
+        fuel_price_monthly.loc[idx,:] = fuel_filt
+
+        hydro_min_filt = hydro_min.loc[day_counter:day_counter+month_day-1,:].mean().values
+        hydro_min_monthly.loc[idx,:] = hydro_min_filt
+
+        hydro_max_filt = hydro_max.loc[day_counter:day_counter+month_day-1,:].mean().values
+        hydro_max_monthly.loc[idx,:] = hydro_max_filt
+
+    day_counter += month_day
+    idx += 1
+
+fuel_price_monthly.to_csv(f"{path_2}/Fuel_prices.csv", index=False)
+hydro_min_monthly.to_csv(f"{path_2}/Hydro_min.csv", index=False)
+hydro_max_monthly.to_csv(f"{path_2}/Hydro_max.csv", index=False)
+
+
+#Reading and organizing nuclear data and copying to simulation folder
+nuclear = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/must_run.csv", header=0)
+nuclear_monthly = pd.DataFrame(np.zeros((len(Days_in_months),hydro_min.shape[1])), columns=hydro_min.columns)
+
+for node in hydro_min.columns:
+
+    try:
+        nuclear_monthly.loc[:,node] = np.repeat(nuclear.loc[0,node],len(Days_in_months))
+
+    except KeyError:
+        nuclear_monthly.loc[:,node] = np.repeat(0,len(Days_in_months))
+
+nuclear_monthly.to_csv(f"{path_2}/must_run.csv", index=False)
+
+#Reading and calculating peak demand, average wind and solar for each month and copying to simulation folder
+load = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/nodal_load.csv", header=0)
+load_monthly = pd.DataFrame(np.zeros((len(Days_in_months),load.shape[1])), columns=load.columns)
+
+offshore_wind = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/nodal_offshorewind.csv", header=0)
+offshore_wind_monthly = pd.DataFrame(np.zeros((len(Days_in_months),offshore_wind.shape[1])), columns=offshore_wind.columns)
+
+onshore_wind = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/nodal_wind.csv", header=0)
+onshore_wind_monthly = pd.DataFrame(np.zeros((len(Days_in_months),onshore_wind.shape[1])), columns=onshore_wind.columns)
+
+solar = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/nodal_solar.csv", header=0)
+solar_monthly = pd.DataFrame(np.zeros((len(Days_in_months),solar.shape[1])), columns=solar.columns)
+
+hour_counter = 0
+idx = 0
+for month_hour in Hours_in_months:
+
+    if idx == 0:
+        load_filt = load.loc[0:month_hour-1,:].max().values
+        load_monthly.loc[idx,:] = load_filt
+
+        offshore_wind_filt = offshore_wind.loc[0:month_hour-1,:].mean().values
+        offshore_wind_monthly.loc[idx,:] = offshore_wind_filt
+
+        onshore_wind_filt = onshore_wind.loc[0:month_hour-1,:].mean().values
+        onshore_wind_monthly.loc[idx,:] = onshore_wind_filt
+
+        solar_filt = solar.loc[0:month_hour-1,:].copy()
+        for node in solar.columns:
+
+            sp_node_sol = solar_filt[node]
+            mean_node_sol = sp_node_sol[sp_node_sol != 0]
+
+            if len(mean_node_sol) == 0:
+                solar_monthly.loc[idx,node] = 0
+
+            else:
+                solar_monthly.loc[idx,node] = mean_node_sol.mean()
+    
+    else:
+
+        load_filt = load.loc[hour_counter:hour_counter+month_hour-1,:].max().values
+        load_monthly.loc[idx,:] = load_filt
+
+        offshore_wind_filt = offshore_wind.loc[hour_counter:hour_counter+month_hour-1,:].mean().values
+        offshore_wind_monthly.loc[idx,:] = offshore_wind_filt
+
+        onshore_wind_filt = onshore_wind.loc[hour_counter:hour_counter+month_hour-1,:].mean().values
+        onshore_wind_monthly.loc[idx,:] = onshore_wind_filt
+
+        solar_filt = solar.loc[hour_counter:hour_counter+month_hour-1,:].copy()
+        for node in solar.columns:
+
+            sp_node_sol = solar_filt[node]
+            mean_node_sol = sp_node_sol[sp_node_sol != 0]
+            
+            if len(mean_node_sol) == 0:
+                solar_monthly.loc[idx,node] = 0
+
+            else:
+                solar_monthly.loc[idx,node] = mean_node_sol.mean()
+
+    hour_counter += month_hour
+    idx += 1
+
+load_monthly.to_csv(f"{path_2}/nodal_load.csv", index=False)
+offshore_wind_monthly.to_csv(f"{path_2}/nodal_offshorewind.csv", index=False)
+onshore_wind_monthly.to_csv(f"{path_2}/nodal_wind.csv", index=False)
+solar_monthly.to_csv(f"{path_2}/nodal_solar.csv", index=False)
+
+#Copying line/node matrix to simulation folder
+copy(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/line_to_bus.csv", path_2)
+
+#Reading existing line parameters
+existing_lines = pd.read_csv(f"Datasets/GO_data/Exp{GO_case_name}/Inputs/line_params.csv", header=0)
+
+#Reading node data
+all_node_IDs = [int(i[4:]) for i in load_monthly.columns]
+
+Node_dataset = pd.read_csv('Datasets/Node_data/10k_Nodes.csv', header=0)
+Node_dataset = Node_dataset.loc[Node_dataset["Number"].isin(all_node_IDs)]
+Node_dataset.reset_index(inplace=True,drop=True)
+geometry = [Point(xy) for xy in zip(Node_dataset['Substation Longitude'],Node_dataset['Substation Latitude'])]
+nodes_gdf = gpd.GeoDataFrame(Node_dataset,crs='epsg:4326',geometry=geometry)
+nodes_gdf = nodes_gdf.to_crs("EPSG:3395")
+nodes_gdf['TPR'] = np.repeat('Unknown',len(nodes_gdf))
+
+#Reading and transmission region shapefile
+Transmission_gdf = gpd.read_file('Datasets/Transmission_Regions_Shapefile/Transmission_Regions_Subregions.shp')
+Transmission_gdf = Transmission_gdf.to_crs("EPSG:3395")
+WECC_tr_gdf = Transmission_gdf.loc[Transmission_gdf['NAME']=='WESTERN ELECTRICITY COORDINATING COUNCIL (WECC)'].copy()
+WECC_tr_gdf.reset_index(inplace=True,drop=True)
+
+#Finding and saving transmision planning region of every node
+for NN in range(0,len(Node_dataset)):
+
+    node_point = nodes_gdf.loc[NN,'geometry']
+    TPR_idx = node_point.within(WECC_tr_gdf['geometry']).idxmax()
+    TPR_name = WECC_tr_gdf.loc[TPR_idx,'SUBNAME']
+
+    if TPR_name == 'CA-MX US':
+        nodes_gdf.loc[NN,'TPR'] = 'CAISO'
+    elif TPR_name == 'NWPP':
+        nodes_gdf.loc[NN,'TPR'] = 'NorthernGrid'
+    else:
+        nodes_gdf.loc[NN,'TPR'] = 'WestConnect'
+
+#Creating columns for types, lengths and costs of transmission lines
+existing_lines['transmission_type'] = np.repeat('Unknown',len(existing_lines))
+existing_lines['length_mile'] = np.repeat(-1.5,len(existing_lines))
+existing_lines['inv_cost_$_per_MWmile'] = np.repeat(-1.5,len(existing_lines))
+
+#Iterating over transmission lines to calculate types, lengths and costs
+for LL in range(0,len(existing_lines)):
+
+    line_name = existing_lines.loc[LL,'line']
+    splitted_name = line_name.split('_')
+
+    #Checking if the line crosses TPR borders or not
+    first_TPR = nodes_gdf.loc[nodes_gdf['Number']==int(splitted_name[1])]['TPR'].values[0]
+    second_TPR = nodes_gdf.loc[nodes_gdf['Number']==int(splitted_name[2])]['TPR'].values[0]
+
+    if first_TPR == second_TPR:
+        line_type = 'regional'
+    else:
+        line_type = 'interregional'
+
+    existing_lines.loc[LL,'transmission_type'] = line_type
+
+    #Calculating the length of line in miles
+    first_coordinates = (nodes_gdf.loc[nodes_gdf['Number']==int(splitted_name[1])]['Substation Latitude'].values[0],
+                         nodes_gdf.loc[nodes_gdf['Number']==int(splitted_name[1])]['Substation Longitude'].values[0])
+    
+    second_coordinates = (nodes_gdf.loc[nodes_gdf['Number']==int(splitted_name[2])]['Substation Latitude'].values[0],
+                         nodes_gdf.loc[nodes_gdf['Number']==int(splitted_name[2])]['Substation Longitude'].values[0])
+    
+    #Adding line length adder to line lengths for security purposes
+    line_length = geodesic(first_coordinates, second_coordinates).miles * (1+(Line_len_security_scalar/100))
+    existing_lines.loc[LL,'length_mile'] = round(line_length, 3)
+
+    #Calculating $/MW-mile cost of each transmission line
+    if line_length < 300: #Equation is y = 1.6875*x + 2671.25
+
+        if line_type == 'regional':
+            line_cost = (1.6875*line_length) + 2671.25
+        else:
+            line_cost = ((1.6875*line_length) + 2671.25) * (1+(Interreginonal_TEP_penalty/100))
+
+        existing_lines.loc[LL,'inv_cost_$_per_MWmile'] = round(line_cost, 3)
+
+    elif line_length >= 300 and line_length < 500: #Equation is y = -6.7*x + 5130
+        
+        if line_type == 'regional':
+            line_cost = (-6.7*line_length) + 5130
+        else:
+            line_cost = ((-6.7*line_length) + 5130) * (1+(Interreginonal_TEP_penalty/100))
+
+        existing_lines.loc[LL,'inv_cost_$_per_MWmile'] = round(line_cost, 3)
+
+    elif line_length >= 500 and line_length < 1000: #Equation is y = -0.55*x + 2045
+
+        if line_type == 'regional':
+            line_cost = (-0.55*line_length) + 2045
+        else:
+            line_cost = ((-0.55*line_length) + 2045) * (1+(Interreginonal_TEP_penalty/100))
+        
+        existing_lines.loc[LL,'inv_cost_$_per_MWmile'] = round(line_cost, 3)
+
+    else: #Equation is y = 1495
+
+        if line_type == 'regional':
+            line_cost = 1495 
+        else:
+            line_cost = 1495 * (1+(Interreginonal_TEP_penalty/100))
+
+        existing_lines.loc[LL,'inv_cost_$_per_MWmile'] = round(line_cost, 3) 
+
+existing_lines.to_csv(f"{path_2}/line_params.csv", index=False)
+
+#Copying ED simulation script to simulation folder
+copy("Model_codes/ED_simulation.py", path_1)
+
+#Copying TEP simulation script to simulation folder
+copy("Model_codes/TEP_simulation.py", path_1)
+
+#Printing the status of the model setup script
+print('Model setup for transmission expansion model is finished.')
+    
